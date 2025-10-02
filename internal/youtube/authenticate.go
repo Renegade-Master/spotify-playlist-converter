7a9842816d9afc0f13c5f4c16dd5c2e535570fd8
Renegade-Master <renegade@renegade-master.com>
2025-10-02 15:54:07 +0000

    Update Copyright;

--- internal/youtube/authenticate.go
+++ internal/youtube/authenticate.go
@@ -1,3 +1,21 @@
+/*
+ * Copyright (c) 2025.
+ *
+ *    Copyright (c) 2025 [renegade@renegade-master.com]
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
 package youtube
 
 import (
@@ -49,14 +67,6 @@ func createYouTubeService() *youtube.Service {
 
 // getClient retrieves a token, saves the token, and returns the configured client.
 func getClient(config *oauth2.Config) *http.Client {
-	//tokFile := "auth/youtube_client_secret.json"
-
-	//tok, err := tokenFromFile(tokFile)
-	//if err != nil {
-	//	tok = getTokenFromWeb(config)
-	//	saveToken(tokFile, tok)
-	//}
-
 	tok := getTokenFromWeb(config)
 	return config.Client(context.Background(), tok)
 }
